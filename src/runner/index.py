924960005a12cec4033265bcba560e40376c2b9e
soufyakoub <splentercell.1997@gmail.com>
2020-05-26 09:32:04 +0000

    Declare rabbitmq hostname as a .env variable

--- src/runner/index.py
+++ src/runner/index.py
@@ -25,7 +25,7 @@ def on_message(channel: Channel, method: Basic.Deliver, props: BasicProperties, 
 
 
 consumer = Consumer(
-    host="rubbitmq",
+    host=environ["RABBITMQ_HOST"],
     queue=environ["TASKS_QUEUE_NAME"].upper(),
     prefetch_value=int(environ["PREFETCH_VALUE"]),
     on_message=on_message,
