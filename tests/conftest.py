5ab626ced230705276cb65a3398d55a59f26efdc
soufyakoub <splentercell.1997@gmail.com>
2020-05-31 04:01:15 +0100

    Define "container" fixture factory in conftest.py
    A temporary solution for [this issue][issue] until a new version of [pytest-docker-tools][repo] is released.
    
    [issue]: https://github.com/Jc2k/pytest-docker-tools/issues/9#issuecomment-634311572
    [repo]: https://github.com/Jc2k/pytest-docker-tools

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,34 @@
-from pytest_docker_tools import container, volume, wrappers
+from pytest_docker_tools import volume, wrappers
+from pytest_docker_tools.builder import fixture_factory
+from pytest_docker_tools.exceptions import ContainerNotReady, TimeoutError
+from pytest_docker_tools.utils import wait_for_callable
+from pytest_docker_tools.wrappers import Container
+
+
+@fixture_factory()
+def container(request, docker_client, wrapper_class, **kwargs):
+    """
+    A temporary solution for this
+    `issue <https://github.com/Jc2k/pytest-docker-tools/issues/9#issuecomment-634311572>`_
+    until a new version of
+    `pytest-docker-tools <https://github.com/Jc2k/pytest-docker-tools>`_
+    is released.
+    """
+    timeout = kwargs.pop('timeout', 30)
+    kwargs.update({'detach': True})
+
+    raw_container = docker_client.containers.run(**kwargs)
+    request.addfinalizer(lambda: raw_container.remove(force=True) and raw_container.wait(timeout=10))
+
+    wrapper_class = wrapper_class or Container
+    container = wrapper_class(raw_container)
+
+    try:
+        wait_for_callable('Waiting for container to be ready', container.ready, timeout)
+    except TimeoutError:
+        raise ContainerNotReady(container, 'Timeout while waiting for container to be ready')
+
+    return container
 
 
 class RabbitMQContainer(wrappers.Container):
